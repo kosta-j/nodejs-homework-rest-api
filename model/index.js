dae4ab276d697aa76ad815fb1778c7285ac5c9ec
Konstantin Zhukov <zhukov.konst.sl@gmail.com>
2021-10-31 07:54:38 +0200

    fix: add path

--- model/index.js
+++ model/index.js
@@ -1,7 +1,8 @@
 const fs = require('fs/promises')
+const path = require('path')
 const crypto = require('crypto')
 
-const contactsPath = require('./contacts.json')
+const contactsPath = path.join(__dirname, './contacts.json')
 
 const readData = async () => {
   const result = await fs.readFile(contactsPath, 'utf8')
