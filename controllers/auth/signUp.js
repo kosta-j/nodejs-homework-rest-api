2a584766fc4eac3dad23a32c9ac02c3d48ee607e
Konstantin Zhukov <zhukov.konst.sl@gmail.com>
2021-12-03 10:03:59 +0200

    add sendMail and verification token to signUp controller

--- controllers/auth/signUp.js
+++ controllers/auth/signUp.js
@@ -1,5 +1,7 @@
 const { User } = require('../../model')
 const gravatar = require('gravatar')
+const { nanoid } = require('nanoid')
+const { sendMail } = require('../../helpers')
 
 const signUp = async (req, res, next) => {
   try {
@@ -9,10 +11,22 @@ const signUp = async (req, res, next) => {
     if (user) {
       return res.status(409).json({ message: 'Email in use' })
     }
+
+    const verificationToken = nanoid()
     const avatarURL = gravatar.url(email, { d: 'identicon' }, false)
-    const newUser = new User({ email, avatarURL })
+
+    const newUser = new User({ email, avatarURL, verificationToken })
     newUser.setPassword(password)
     await newUser.save()
+
+    // verification email sending:
+    const mail = {
+      to: email,
+      subject: 'Signup confirmation',
+      text: `<a href="http://localhost:3000/api/users/verify/${verificationToken}">Click to confirm your registration</a>`,
+    }
+    await sendMail(mail)
+
     res.status(201).json({
       user: {
         email,
